dca9bfa9ebac88fee1d545a40c7e526ecf673090
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-18 15:44:58 +0900

    add bonus file(WIP)

---
+++ get_next_line_bonus.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 11:07:44 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/11/18 15:44:38 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/**
+ * @brief Write a function that returns a line read from a file descriptor
+ * @param [in] fd	: file descriptor
+ * @retval char* 	: Read line
+ * @retval NULL		: there is nothing else to read, or an error occurred
+ */
+char	*get_next_line(int fd)
+{
+	t_string	line;
+	char		c;
+
+	line.str = NULL;
+	line.capa = 0;
+	line.len = 0;
+	while (1)
+	{
+		c = ft_getc(fd);
+		if (c == EOF)
+			break ;
+		ft_putc(&line, c);
+		if (c == '\n')
+			break ;
+	}
+	if (line.len > 0)
+		ft_putc(&line, '\0');
+	return (line.str);
+}
