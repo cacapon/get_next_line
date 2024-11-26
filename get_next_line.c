8d45021d37369823553b795ca27f39fb466d6fd1
cacapon <takuma.tsubo@amazingengine.co.jp>
2024-11-26 13:40:44 +0900

    再設計

--- get_next_line.c
+++ get_next_line.c
@@ -5,69 +5,146 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 11:07:44 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/21 13:20:37 by ttsubo           ###   ########.fr       */
+/*   Created: 2024/11/26 10:21:00 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/11/26 13:24:35 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void	_gnl_init(t_string *line)
+/**
+ * @brief fdの構造体が存在するかチェックします
+ *
+ * @param fd 			: 存在するか確認したいファイルディスクリプタ
+ * @param fd_data		: ファイルディスクリプタの構造体
+ * @retval FD_EXIST		: 構造体が存在します(実質1)
+ * @retval FD_NOT_EXIST	: 構造体が存在しません(実質0)
+ *
+ */
+static t_fd_exist	_is_fd_exist(int fd, t_fd_info *fd_data)
 {
-	line->str = NULL;
-	line->capa = 0;
-	line->len = 0;
-}
+	t_fd_info	*now_node;
 
-static int	_gnl_exec(t_fd_state **fd_list, t_fd_state *state, t_string *line,
-		char *c)
-{
-	*c = ft_getc(state);
-	if (*c == COULD_NOT_READ)
+	if (!fd_data)
+		return (FD_NOT_EXIST);
+	now_node = fd_data;
+	while (now_node->next)
 	{
-		remove_fd_node(fd_list, state->fd);
-		return (IS_BREAK);
+		if (now_node->fd == fd)
+			return (FD_EXIST);
+		now_node = now_node->next;
 	}
-	if (ft_putc(line, *c) == -1)
-	{
-		remove_fd_node(fd_list, state->fd);
-		free(line->str);
-		return (IS_NULL);
-	}
-	if (*c == '\n')
-		return (IS_BREAK);
-	return (0);
+	return (FD_NOT_EXIST);
 }
 
 /**
- * @brief Write a function that returns a line read from a file descriptor
- * @param [in] fd	: file descriptor
- * @retval char* 	: Read line
- * @retval NULL		: there is nothing else to read, or an error occurred
+ * @brief fdの領域を初期化します。既に存在する場合は何もしません。
+ *
+ * @param fd 			: ファイルディスクリプタ
+ * @param fd_info		: ファイルディスクリプタで管理する情報の構造体
+ * @retval e_status OK	: 正常終了
+ * @retval e_status NG	: メモリ割り当てに失敗した
+ */
+static t_status	_fd_info_init(int fd, t_fd_info *fd_info)
+{
+	t_fd_info	*new_info;
+
+	if (_is_fd_exist(fd, fd_info))
+		return (STATUS_OK);
+	new_info = malloc(sizeof(t_fd_info));
+	if (!new_info)
+		return (STATUS_NG);
+	new_info->fd = fd;
+	new_info->buf = malloc(BUFFER_SIZE);
+	if (!new_info->buf)
+		return (STATUS_NG);
+	new_info->buf_len = 0;
+	new_info->line = NULL;
+	new_info->line_len = 0;
+	new_info->line_capa = 0;
+	new_info->next = fd_info;
+	return (STATUS_OK);
+}
+
+/**
+ * @brief baseにあるfdと一致するノードをnodeに格納します
+ *
+ * @param [in] fd 		: ファイルディスクリプタ
+ * @param [in] base		: ファイルディスクリプタ情報の連結リスト
+ * @param [out] node 	: 一致したノードを格納する変数
+ * @retval STATUS_OK	: fdの構造体が見つかった
+ * @retval STATUS_NG	: fdの構造体が見つからなかった
+ */
+static t_status	_get_fd_info(int fd, t_fd_info *base, t_fd_info *node)
+{
+	t_fd_info	*check;
+
+	check = base;
+	while (check)
+	{
+		if (check->fd == fd)
+		{
+			node = check;
+			return (STATUS_OK);
+		}
+		check = check->next;
+	}
+	return (STATUS_NG);
+}
+
+/**
+ * @brief	指定したファイルディスクリプタの構造体を削除します
+ *
+ * @param [in]	fd		: ファイルディスクリプタ
+ * @param [out]	fd_list	: fd構造体のリスト
+ * @note
+ * 	内部変数currentで二重ポインタを使っているのは
+ * 	通常処理が変わる単一リンクリストの削除処理を
+ *  一貫した処理で行える様にするため
+ */
+static void	*_remove_fd_node(int fd, t_fd_info **fd_list)
+{
+	t_fd_info	**current;
+	t_fd_info	*to_free;
+
+	current = fd_list;
+	while (*current)
+	{
+		if ((*current)->fd == fd)
+		{
+			to_free = *current;
+			*current = (*current)->next;
+			free(to_free);
+			break ;
+		}
+		current = &(*current)->next;
+	}
+	return (NULL);
+}
+
+/**
+ * @brief fdから取得したテキストを改行文字を含んで取得します
+ * @param [in] fd	: ファイルディスクリプタ
+ * @retval char* 	: 読み込んだ1行の文字列(\n含む)
+ * @retval NULL		: 中身が無くて読み込めなかったかエラーが起きた
  */
 char	*get_next_line(int fd)
 {
-	static t_fd_state	*fd_list = NULL;
-	t_fd_state			*state;
-	t_string			line;
-	char				c;
-	int					result;
+	static t_fd_info	*fd_info_list = NULL;
+	t_fd_info			*now_fd_info;
 
-	if (fd < 0)
+	if (_fd_info_init(fd, fd_info_list) == STATUS_NG)
 		return (NULL);
-	state = get_fd_node(&fd_list, fd);
-	if (!state)
-		return (NULL);
-	_gnl_init(&line);
+	if (_get_fd_info(fd, fd_info_list, now_fd_info) == STATUS_NG)
+		return (_remove_fd_node(fd, fd_info_list));
 	while (1)
 	{
-		result = _gnl_exec(&fd_list, state, &line, &c);
-		if (result == IS_NULL)
-			return (NULL);
-		if (result == IS_BREAK)
+		if (ft_getbuf(now_fd_info) == STATUS_NG)
+			return (_remove_fd_node(fd, fd_info_list));
+		if (ft_putline(now_fd_info) == STATUS_NG)
+			return (_remove_fd_node(fd, fd_info_list));
+		if (ft_contain_linebreak(fd_info_list) == STATUS_OK)
 			break ;
 	}
-	if (line.len > 0)
-		ft_putc(&line, '\0');
-	return (line.str);
+	return (fd_info_list->line);
 }
--- get_next_line_util.c
+++ get_next_line_util.c
@@ -5,79 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 11:08:41 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/21 13:22:11 by ttsubo           ###   ########.fr       */
+/*   Created: 2024/11/26 12:51:29 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/11/26 13:40:01 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /**
- * @brief 渡されたfdのfd_state構造体を取得します(無ければ新規作成)
- *
- * @param [out]	fd_list	: fd_stateのリスト
- * @param [in]	fd		: ファイルディスクプリタ
- * @return t_fd_state*	: 指定したfdの構造体を返します
- */
-t_fd_state	*get_fd_node(t_fd_state **fd_list, int fd)
-{
-	t_fd_state	*current;
-	t_fd_state	*new_node;
-
-	current = *fd_list;
-	while (current)
-	{
-		if (current->fd == fd)
-			return (current);
-		current = current->next;
-	}
-	new_node = malloc(sizeof(t_fd_state));
-	if (!new_node)
-		return (NULL);
-	new_node->fd = fd;
-	new_node->n = 0;
-	new_node->bufp = NULL;
-	new_node->next = *fd_list;
-	*fd_list = new_node;
-	return (new_node);
-}
-
-/**
- * @brief	指定したファイルディスクリプタの構造体を削除します
- *
- * @param [out]	fd_state_list	: fd構造体のリスト
- * @param [in]	fd				: ファイルディスクリプタ
- * @note
- * 	内部変数currentで二重ポインタを使っているのは
- * 	通常処理が変わる単一リンクリストの削除処理を
- *  一貫した処理で行える様にするため
- */
-void	remove_fd_node(t_fd_state **fd_list, int fd)
-{
-	t_fd_state	**current;
-	t_fd_state	*to_free;
-
-	current = fd_list;
-	while (*current)
-	{
-		if ((*current)->fd == fd)
-		{
-			to_free = *current;
-			*current = (*current)->next;
-			free(to_free);
-			return ;
-		}
-		current = &(*current)->next;
-	}
-}
-
-/**
- * @brief Copy src to dst up to strsize characters.
- *
- * @param [out] dst		: Destination memory address
- * @param [in]	src 	: Source string
- * @param [in]	srcsize	: Number of characters to copy
- * @return char* 		: dst's top pointer
+ * @brief srcの文字列をsrcsizeまでdstにコピーします
+ * 
+ * @param dst 		: コピー先の文字列用の領域
+ * @param src 		: コピーする文字列
+ * @param srcsize	: srcのサイズ
+ * @return char*	: dstの先頭ポインタ
  */
 static char	*_strncpy(char *dst, const char *src, size_t srcsize)
 {
@@ -90,51 +31,51 @@ static char	*_strncpy(char *dst, const char *src, size_t srcsize)
 }
 
 /**
- * @brief Returns one character read from fd for BUFFER_SIZE.
- * @details
- * 	The remaining characters are retained.
- * @param [in] fd 			: file descriptor
- * @retval int				: One character read
- * @retval COULD_NOT_READ	:	Couldn't read it
+ * @brief fd_infoのlineにbufの内容をセットします。
+ * 
+ * @param fd_info 
+ * @return t_status 
  */
-int	ft_getc(t_fd_state *state)
+t_status	ft_putline(t_fd_info *fd_info)
 {
-	if (!state)
-		return (COULD_NOT_READ);
-	if (state->n == 0)
+	char *tmp;
+	if (fd_info->line_capa < fd_info->line_len + fd_info->buf_len)
 	{
-		state->n = read(state->fd, state->buf, BUFFER_SIZE);
-		if (state->n <= 0)
-			return (COULD_NOT_READ);
-		state->bufp = state->buf;
+		fd_info->line_capa = (fd_info->line_len + fd_info->buf_len) * 2;
+		tmp = malloc(fd_info->line_capa);
+		if (!tmp)
+			return (STATUS_NG);
+		tmp = _ft_strncpy(tmp, fd_info->line, fd_info->line_len);
+		free(fd_info->line);
+		fd_info->line = tmp;
 	}
-	state->n--;
-	return ((unsigned char)*state->bufp++);
+	while (fd_info->buf_len < 0 && *(fd_info->buf) == '\0')
+	{
+		fd_info->line[fd_info->line_len] = *fd_info->buf++;
+		fd_info->line_len++;
+		fd_info->buf_len--;
+		if (*(fd_info->buf) == '\n')
+			break;
+	}
+	return (STATUS_OK);
 }
 
 /**
- * @brief Adds the character c to the end of the string str.
- *
- * @param [out]	str	: The string to which the character c is added.
- * @param [in]	c	: Characters you want to add.
- * @retval 0~127	: c ASCII numbers.
- * @retval -1		: Failed to allocate.
+ * @brief バッファがからの場合,readを使ってbufにセットします。
+ * 
+ * @param [out]	fd_info	: fdの情報を管理している構造体
+ * @retval STATUS_OK	: 正常に読み込めた・読み込む必要がなかった
+ * @retval STATUS_NG	: fdから読み込めない・readしたが空だった
  */
-int	ft_putc(t_string *str, char c)
+t_status	ft_getbuf(t_fd_info *fd_info)
 {
-	char	*tmp;
-
-	if (str->len + 1 >= str->capa)
+	if (!fd_info)
+		return (STATUS_NG);
+	if (fd_info->buf_len == 0)
 	{
-		str->capa = (str->len + 1) * 2;
-		tmp = malloc(str->capa);
-		if (!tmp)
-			return (-1);
-		tmp = _strncpy(tmp, str->str, str->len);
-		free(str->str);
-		str->str = tmp;
+		fd_info->buf_len = read(fd_info->fd, fd_info->buf, BUFFER_SIZE);
+		if (fd_info->buf_len <= 0)
+			return (STATUS_NG);
 	}
-	str->str[str->len] = c;
-	str->len++;
-	return (c);
+	return (STATUS_OK);
 }
