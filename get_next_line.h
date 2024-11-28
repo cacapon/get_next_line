e1663ce2bbb2b75bde2c83a111f5a481b4d5637e
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-28 16:54:05 +0900

    コメント追加

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:08:36 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/28 16:38:22 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/28 16:53:42 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ typedef enum e_putc_status
 	PUTC_SUCCESS = 1,
 }						t_putc_status;
 
+// struct
 typedef struct s_fd_buffer
 {
 	int					fd;
@@ -59,6 +60,7 @@ t_status				delete_fd_node(t_fd_buffer **head, int fd);
 t_fd_buffer				*find_fd_node(t_fd_buffer *head, int fd);
 t_fd_buffer				*setup_fd_buffer(int fd, t_fd_buffer **fd_list);
 
+// get_next_line
 t_putc_status			ft_getc(t_fd_buffer *fd_buf, unsigned char *cp);
 int						ft_putc(t_string *str, char c);
 char					*get_next_line(int fd);
