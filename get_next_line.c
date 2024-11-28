e69b79dff8d470c5d3e08f5508cd0ee6c2da9593
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-11-28 12:26:19 +0900

    構造体の初期化を一行にまとめる

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,29 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:07:44 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/11/28 12:13:32 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/11/28 12:19:42 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// TODO: ft_getcが持っているfdの管理をget_next_line側で持つ
+// TODO: stateの連結リスト化
+// TODO: BUFをヒープで管理するようにする
+
+
 /**
- * @brief Write a function that returns a line read from a file descriptor
- * @param [in] fd	: file descriptor
- * @retval char* 	: Read line
- * @retval NULL		: there is nothing else to read, or an error occurred
+ * @brief fdのテキストのうち、改行までの文字列を返します。
+ * @param [in] fd	: ファイルディスクリプタ
+ * @retval char* 	: 読み込んだ行
+ * @retval NULL		: 読めなかった・もしくはエラーだった場合
  */
 char	*get_next_line(int fd)
 {
 	t_string	line;
 	char		c;
 
-	line.str = NULL;
-	line.capa = 0;
-	line.len = 0;
+	line = (t_string){.str = NULL, .capa = 0, .len = 0};
 	if (fd < 0)
 		return (NULL);
 	while (1)
